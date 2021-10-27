8cf20fb811a8ca790edf78b61b2eacb1b6547d7f
Reema8 <reema.bhandari8@gmail.com>
2021-10-27 18:18:49 +0530

    Roman Numerals Function

--- RomanConverter/src/RomanNumeral.js
+++ RomanConverter/src/RomanNumeral.js
@@ -1,1 +1,70 @@
-Initial Setup.
+/* Requirement:
+ 
+  * generateRoman
+  *
+  * Generates a roman numeral when passed an integer
+  *
+  * Starts at the largest number and subtracts from the roman numeral array.
+  * Continues while the number is greater than the value that is being looked up
+  *
+  * Example inputs and results:
+  * 1 = I
+  * 5 = V
+  * 10 = X
+  * 20 = XX
+  * 3999 = MMMCMXCIX
+  *
+  */
+  
+const Main_Array = [
+  [1000, 'M'],
+  [900, 'CM'],
+  [500, 'D'],
+  [400, 'CD'],
+  [100, 'C'],
+  [90, 'XC'],
+  [50, 'L'],
+  [40, 'XL'],
+  [10, 'X'],
+  [9, 'IX'],
+  [5, 'V'],
+  [4, 'IV'],
+  [1, 'I']
+]
+
+/*
+* RomanNumeralGenerator main class with constructor in which passing number as a parameter 
+* number(which is later converting into integer by using unary(+) operator)
+* Purpose is to generate a roman numeral from a provided integer
+*/
+export default class RomanNumeralGenerator {
+  constructor(number) {
+    this.number = +number;
+    this.mainArray = Main_Array;
+  }
+
+  generateRoman() {
+    let num = this.number;
+    let result = '';
+	
+	// NaN: returns true, if number is not a number
+    if (isNaN(num)) {
+      return "Error: Please enter a valid number";
+    }
+	
+    // Used .map for iteration in place of for loop.
+    this.mainArray.map(item => {
+      let numDecimal = item[0]; //item[0] is the decimal, e.g. 10
+      let numNumeral = item[1]; //item[1] is the numeral, e.g. X
+      // while number is divisible (mod) by current decimal without a remainder greater than itself
+      while (num % numDecimal < num) {
+        // add the current roman numeral to our result string
+        result += numNumeral;
+        // remove the current decimal from our number
+        num -= numDecimal;
+      }
+    });
+    // Return the final result string
+    return result;
+  }
+}
--- RomanConverter/test/RomanNumeral.spec.js
+++ RomanConverter/test/RomanNumeral.spec.js
@@ -1,1 +1,79 @@
-// Initial setup for test
+const expect = require('chai').expect; // 
+import RomanNumeralGenerator from '../src/RomanNumeral'; // Import our class itself
+
+describe('The RomanNumeralGenerator', () => {
+  let romanNumeralGenerator;
+
+  /*
+  * Mount an instance of the RomanNumeralGenerator before each test run
+  * we may override this if we want to use different values
+  */
+  beforeEach(() => {
+    romanNumeralGenerator = new RomanNumeralGenerator(8);
+  });
+
+  /*
+  * First test that an instance of the class itself has been created
+  */
+  it('instance can be created', () => {
+    expect(romanNumeralGenerator.number).to.be.equal(8);
+  })
+
+  /*
+  * Test a number that only requires addition of strings, e.g. 8
+  * should equal VIII
+  */
+  it('should return a roman numeral when passed an integer', () => {
+      let result = romanNumeralGenerator.generateRoman();
+      expect(result).to.be.equal("VIII");
+  });
+
+  /*
+  * Test a number that also requires taking away strings, e.g. 4
+  * should equal IV
+  */
+  it('should return a roman numeral when passed an integer', () => {
+      romanNumeralGenerator = new RomanNumeralGenerator(4);
+      let result = romanNumeralGenerator.generateRoman();
+      expect(result).to.be.equal("IV");
+  });
+
+  /*
+  * Test a larger number, e.g. 3999
+  * should equal MMMCMXCIX
+  */
+  it('should return a roman numeral when passed an integer', () => {
+      romanNumeralGenerator = new RomanNumeralGenerator(3999);
+      let result = romanNumeralGenerator.generateRoman();
+      expect(result).to.be.equal("MMMCMXCIX");
+  });
+
+  /*
+  * Test a an invalid input
+  * should return an error
+  */
+  it('should return error when passed an invalid input, e.g. string', () => {
+      romanNumeralGenerator = new RomanNumeralGenerator("qwerty");
+      let result = romanNumeralGenerator.generateRoman();
+      expect(result).to.be.equal("Error: Please enter a valid number");
+  });
+
+  /*
+  * Test a number passed as a string, should be converted to an int
+  */
+  it('should return a roman numeral when passed a string number', () => {
+      romanNumeralGenerator = new RomanNumeralGenerator("10");
+      let result = romanNumeralGenerator.generateRoman();
+      expect(result).to.be.equal("X");
+  });
+
+  /*
+  * Test a larger number, e.g. 3999
+  * should equal MMMCMXCIX
+  */
+  it('should return a roman numeral when passed an ', () => {
+      romanNumeralGenerator = new RomanNumeralGenerator(3999);
+      let result = romanNumeralGenerator.generateRoman();
+      expect(result).to.be.equal("MMMCMXCIX");
+  });
+});
