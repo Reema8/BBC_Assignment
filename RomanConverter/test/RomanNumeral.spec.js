308874f1a87995d6a7c4ebf43a8af75e768882f4
Reema8 <reema.bhandari8@gmail.com>
2021-10-27 18:59:33 +0530

    Removed extra test cases

--- RomanConverter/test/RomanNumeral.spec.js
+++ RomanConverter/test/RomanNumeral.spec.js
@@ -5,40 +5,6 @@ describe('The RomanNumeralGenerator', () => {
   let romanNumeralGenerator;
 
   /*
-  * Mount an instance of the RomanNumeralGenerator before each test run
-  * we may override this if we want to use different values
-  */
-  beforeEach(() => {
-    romanNumeralGenerator = new RomanNumeralGenerator(8);
-  });
-
-  /*
-  * First test that an instance of the class itself has been created
-  */
-  it('instance can be created', () => {
-    expect(romanNumeralGenerator.number).to.be.equal(8);
-  })
-
-  /*
-  * Test a number that only requires addition of strings, e.g. 8
-  * should equal VIII
-  */
-  it('should return a roman numeral when passed an integer', () => {
-      let result = romanNumeralGenerator.generateRoman();
-      expect(result).to.be.equal("VIII");
-  });
-
-  /*
-  * Test a number that also requires taking away strings, e.g. 4
-  * should equal IV
-  */
-  it('should return a roman numeral when passed an integer', () => {
-      romanNumeralGenerator = new RomanNumeralGenerator(4);
-      let result = romanNumeralGenerator.generateRoman();
-      expect(result).to.be.equal("IV");
-  });
-
-  /*
   * Test a larger number, e.g. 3999
   * should equal MMMCMXCIX
   */
@@ -65,15 +31,5 @@ describe('The RomanNumeralGenerator', () => {
       romanNumeralGenerator = new RomanNumeralGenerator("10");
       let result = romanNumeralGenerator.generateRoman();
       expect(result).to.be.equal("X");
-  });
-
-  /*
-  * Test a larger number, e.g. 3999
-  * should equal MMMCMXCIX
-  */
-  it('should return a roman numeral when passed an ', () => {
-      romanNumeralGenerator = new RomanNumeralGenerator(3999);
-      let result = romanNumeralGenerator.generateRoman();
-      expect(result).to.be.equal("MMMCMXCIX");
   });
 });
